bdfe321368893df11db290962cf970214d091812
AmeliaToposzkiewicz <ameliatoposzkiewicz@gmail.com>
2023-07-17 20:32:54 +0200

    feature - creating Product Entity and Dto

---
+++ src/main/java/com/sportstore/entity/Product.java
@@ -0,0 +1,34 @@
+package com.sportstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+    private BigDecimal price;
+
+    public Product(String name, String description, String image, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+}
