0d7157f8afce9243886c40d16ae0fb81669db08f
AmeliaToposzkiewicz <ameliatoposzkiewicz@gmail.com>
2023-07-18 19:47:06 +0200

    feature - import images and insert data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into product(name, price, image, description)
+values('Ciężarki', 24.99, 'dumbbells.png', 'Zestaw 3 ciężarków: 5, 10 i 15 kilogramów. Kolor: czarny.');
+
+insert into product(name, price, image, description)
+values('Piłka do piłki nożnej', 59.95, 'football-ball.png', 'Czerwono-czarna piłka do piłki nożnej.');
+
+insert into product(name, price, image, description)
+values('Mata', 35.50, 'gym-mat.png', 'Fioletowa mata idealna na siłownię lub jogę.');
+
+insert into product(name, price, image, description)
+values('Skakanka', 9.99, 'jumping-rope.png', 'Biała skakanka z zielonymi rączkami. Długość odpowiednia dla dorosłych.');
+
+insert into product(name, price, image, description)
+values('Skarpetki', 47.75, 'socks.png', 'Zestaw 3 par skarpet: białe, szare i czarne.');
