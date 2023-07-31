e0ea25a6c5c17cad46ac0e55b665f4816a55d5b4
AmeliaToposzkiewicz <ameliatoposzkiewicz@gmail.com>
2023-07-31 17:47:08 +0200

    feature - security configuration

---
+++ src/main/java/com/example/sportstore/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.sportstore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(req -> req.requestMatchers("/products", "/products/**", "/uploads/**", "/cart/**")
+                        .permitAll().requestMatchers("/admin/**").hasAuthority("ADMIN"))
+                .formLogin(form -> form.loginPage("/login").permitAll());
+
+        http.logout(logout -> logout.logoutSuccessUrl("/products"));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
