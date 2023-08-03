369a47a9f4e6a93872b953755717d1d3be2032b0
AmeliaToposzkiewicz <ameliatoposzkiewicz@gmail.com>
2023-08-03 20:34:08 +0200

    feature - view order details

--- src/main/java/com/example/sportstore/controller/OrderController.java
+++ src/main/java/com/example/sportstore/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.example.sportstore.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +24,13 @@ public class OrderController {
         List<OrderDto> ordersFromDb = orderService.getOrders();
         model.addAttribute("orders", ordersFromDb);
         return "orders";
+    }
+
+    @GetMapping
+    @RequestMapping("/details/{orderId}")
+    public String orderDetails(Model model, @PathVariable long orderId) {
+        OrderDto orderDto = orderService.getOrderById(orderId);
+        model.addAttribute("order", orderDto);
+        return "order-details";
     }
 }
--- src/main/java/com/example/sportstore/service/OrderService.java
+++ src/main/java/com/example/sportstore/service/OrderService.java
@@ -5,6 +5,7 @@ import com.example.sportstore.dto.OrderDto;
 import com.example.sportstore.dto.ProductDto;
 import com.example.sportstore.entity.Order;
 import com.example.sportstore.entity.Product;
+import com.example.sportstore.exception.ObjectNotFoundException;
 import com.example.sportstore.repository.OrderRepository;
 import com.example.sportstore.repository.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -35,5 +36,12 @@ public class OrderService {
     public List<OrderDto> getOrders() {
         return orderRepository.findAll().stream().map(o -> new OrderDto(o.getId(), o.getCustomerFullName(), o.getCustomerEmail(),
                 o.getCity(), o.getZipCode(), o.getStreet(), o.getStreetNo(), o.getHomeNo(), o.getPrice().toString())).toList();
+    }
+
+    public OrderDto getOrderById(Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ObjectNotFoundException("Order with id: " + orderId + " not found"));
+        return new OrderDto(order.getId(), order.getCustomerFullName(), order.getCustomerEmail(), order.getCity(),
+                order.getZipCode(), order.getStreet(), order.getStreetNo(), order.getHomeNo(), order.getPrice().toString());
     }
 }
