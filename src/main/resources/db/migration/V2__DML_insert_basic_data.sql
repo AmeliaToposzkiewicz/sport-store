3ba6eb89f5164c305681e0121c8ca49d5d0275ab
AmeliaToposzkiewicz <ameliatoposzkiewicz@gmail.com>
2023-08-07 19:37:19 +0200

    feature - added flyway

---
+++ src/main/resources/db/migration/V2__DML_insert_basic_data.sql
@@ -0,0 +1,17 @@
+insert into product(name, price, image, description)
+values('Czepek', 24.99, 'swimming-cap.png', 'Czepek do pływania dla dorosłych. Kolor: niebieski.');
+
+insert into product(name, price, image, description)
+values('Piłka do piłki nożnej', 59.95, 'football-ball.png', 'Czerwono-czarna piłka do piłki nożnej.');
+
+insert into product(name, price, image, description)
+values('T-shirt', 35.50, 't-shirt.png', 'Czarna prosta koszulka z bawełny.');
+
+insert into product(name, price, image, description)
+values('Skakanka', 9.99, 'jumping-rope.png', 'Biała skakanka z zielonymi rączkami. Długość odpowiednia dla dorosłych.');
+
+insert into product(name, price, image, description)
+values('Skarpetki', 47.75, 'socks.png', 'Zestaw 3 par skarpet: białe, szare i czarne.');
+
+insert into user (username, password, enabled, authority)
+values ('admin', '$2a$12$GIufZ0iBA8yH9kZoa07YcehYs8xnzzmBbtL3Er6xeuosAnTya7Wqm', 1, 'ADMIN');
